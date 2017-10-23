dfd841c2a53606614b69175535c87e1b1739749c
smeeklai <wizer_boss@hotmail.com>
2017-10-23 14:07:11 +0700

    enable local db connection

--- myapp/main.go
+++ myapp/main.go
@@ -20,23 +20,36 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 
-	"github.com/smeeklai/gizlinebot/storage"
 	"github.com/smeeklai/gizlinebot/line"
-
+	"github.com/smeeklai/gizlinebot/storage"
 )
 
 func init() {
-	connectionName := mustGetenv("CLOUDSQL_CONNECTION_NAME")
-	user := mustGetenv("CLOUDSQL_USER")
-	password := os.Getenv("CLOUDSQL_PASSWORD") // NOTE: password may be empty
-	dbName := mustGetenv("CLOUDSQL_DB")
-
-	s, err := storage.NewGoogleSql(user, password, connectionName, dbName)
+	connectionName := mustGetenv("GIZLB_SQL_HOST")
+	user := mustGetenv("GIZLB_SQL_USER")
+	password := os.Getenv("GIZLB_SQL_PASS") // NOTE: password may be empty
+	dbName := mustGetenv("GIZLB_SQL_DB")
+	dbPort := os.Getenv("GIZLB_SQL_PORT") // NOTE: dbPort may be empty
+	localDB, err := strconv.ParseBool(mustGetenv("LOCAL_DB"))
 	if err != nil {
-		fmt.Sprintf("\nCould not open db: %v", err)
+		fmt.Sprintf("\nLOCAL_DB value error")
+	}
+	var s *Sql
+	var err error
+	if localDB {
+		s, err = storage.NewSql(user + ":" + password + "@(" + connectionName + ":" + dbPort + ")/" + dbName)
+		if err != nil {
+			fmt.Sprintf("\nCould not open db: %v", err)
+		}
+	} else {
+		s, err = storage.NewGoogleSql(user, password, connectionName, dbName)
+		if err != nil {
+			fmt.Sprintf("\nCould not open db: %v", err)
+		}
 	}
 
 	err = line.ServeAppEngine(s, mustGetenv("CHANNEL_SECRET"), mustGetenv("CHANNEL_TOKEN"))
@@ -48,7 +61,7 @@ func init() {
 func mustGetenv(k string) string {
 	v := os.Getenv(k)
 	if v == "" {
-			log.Panicf("%s environment variable not set.", k)
+		log.Panicf("%s environment variable not set.", k)
 	}
 	return v
 }
