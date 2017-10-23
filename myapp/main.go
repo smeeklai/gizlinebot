4ad169f65ddc8f1bb66c3a92eca7e48db8f4e846
smeeklai <wizer_boss@hotmail.com>
2017-10-23 15:49:21 +0700

    updated readme

--- myapp/main.go
+++ myapp/main.go
@@ -1,19 +1,3 @@
-// Copyright 2016 LINE Corporation
-//
-// LINE Corporation licenses this file to you under the Apache License,
-// version 2.0 (the "License"); you may not use this file except in compliance
-// with the License. You may obtain a copy of the License at:
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations
-// under the License.
-
-// +build appengine
-
 package myapp
 
 import (
@@ -21,8 +5,6 @@ import (
 	"log"
 	"os"
 	"strconv"
-
-	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/smeeklai/gizlinebot/line"
 	"github.com/smeeklai/gizlinebot/storage"
@@ -38,23 +20,25 @@ func init() {
 	if err != nil {
 		fmt.Sprintf("\nLOCAL_DB value error")
 	}
-	var s *Sql
-	var err error
+
 	if localDB {
-		s, err = storage.NewSql(user + ":" + password + "@(" + connectionName + ":" + dbPort + ")/" + dbName)
+		s, err := storage.NewSql(user + ":" + password + "@(" + connectionName + ":" + dbPort + ")/" + dbName)
 		if err != nil {
 			fmt.Sprintf("\nCould not open db: %v", err)
+		}
+		err = line.ServeAppEngine(s, mustGetenv("CHANNEL_SECRET"), mustGetenv("CHANNEL_TOKEN"))
+		if err != nil {
+			fmt.Sprintf("\nError: %s\n", err)
 		}
 	} else {
-		s, err = storage.NewGoogleSql(user, password, connectionName, dbName)
+		s, err := storage.NewGoogleSql(user, password, connectionName, dbName)
 		if err != nil {
 			fmt.Sprintf("\nCould not open db: %v", err)
 		}
-	}
-
-	err = line.ServeAppEngine(s, mustGetenv("CHANNEL_SECRET"), mustGetenv("CHANNEL_TOKEN"))
-	if err != nil {
-		fmt.Sprintf("\nError: %s\n", err)
+		err = line.ServeAppEngine(s, mustGetenv("CHANNEL_SECRET"), mustGetenv("CHANNEL_TOKEN"))
+		if err != nil {
+			fmt.Sprintf("\nError: %s\n", err)
+		}
 	}
 }
 
